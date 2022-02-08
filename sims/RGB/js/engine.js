f2a89eff566528586473f3697912d19ce47c8fa2
karpych1717 <admkarpych1717@gmail.com>
2022-02-08 21:39:00 +0200

    add rotation speed

--- sims/RGB/js/engine.js
+++ sims/RGB/js/engine.js
@@ -10,24 +10,60 @@ cvs.width  = BoxWidth;
 cvs.style.border  = '2px solid green';
 
 
-//drawing
-const RGBPic = new Image();
-RGBPic.src  = 'img/RGB_3.png';
+let speed = 1;
+const speedRange = document.getElementById('speed');
+speedRange.value = speed;
 
-const draw = () => {
-    ctx.drawImage(RGBPic, 100, 100, 300, 300);
+speedRange.oninput = () => {
+    speed = speedRange.value;
 };
 
+//circle parameters
+let angle = 0;
+
+let color_1 = 'rgb(255, 0, 0)';
+let color_2 = 'rgb(0, 255, 0)';
+let color_3 = 'rgb(0, 0, 255)';
+
+let cx = 250;
+let cy = 250;
+let r = 150;
+
+
+
+const drawCircle = () => {
+    ctx.fillStyle = color_1;
+    ctx.beginPath();
+    ctx.moveTo(cx, cy);
+    ctx.arc(cx, cy, r, angle + 0, angle + Math.PI * 2 / 3);
+    ctx.lineTo(cx, cy);
+    ctx.fill();
+
+    ctx.fillStyle = color_2;
+    ctx.beginPath();
+    ctx.moveTo(cx, cy);
+    ctx.arc(cx, cy, r, angle + Math.PI * 2 / 3, angle + Math.PI * 4 / 3);
+    ctx.lineTo(cx, cy);
+    ctx.fill();
+
+    ctx.fillStyle = color_3;
+    ctx.beginPath();
+    ctx.moveTo(cx, cy);
+    ctx.arc(cx, cy, r, angle + Math.PI * 4 / 3, angle + Math.PI * 2);
+    ctx.lineTo(cx, cy);
+    ctx.fill();
+};
 
 function render() {
-    ctx.translate(250, 250);
-    ctx.rotate(Math.PI / 3*2);
-    ctx.translate(-250, -250);
+    angle += speed / 180 * Math.PI;
 
-    draw();
+    if (angle > 2 * Math.PI) {
+        angle = angle - 2 * Math.PI;
+    }
+
+    drawCircle();
 
     requestAnimationFrame(render);
 }
 
-RGBPic.addEventListener('load', draw);
-RGBPic.addEventListener('load', render);
+render();

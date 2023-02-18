c3f4492fd30b9c3f0e95155991f9791a7922fb71
karpych1717 <admkarpych1717@gmail.com>
2023-02-18 20:58:44 +0200

    add basic logic

--- sims/RGB/js/core.js
+++ sims/RGB/js/core.js
@@ -31,8 +31,10 @@ class Stage {
 
     drawAll() {
         this.toEvolve.forEach(obj => {
-            obj.drawIt()
+            obj.drawIt(this.pre_ctx)
         })
+
+        this.ctx.drawImage(pre_cvs, 0, 0);
     }
 
     render() {
@@ -42,3 +44,67 @@ class Stage {
         requestAnimationFrame(this.render);
     }
 }
+
+
+class Circle {
+    constructor() {
+        this.first  = {red: 255, green: 0,   blue: 0,};
+        this.second = {red: 0,   green: 255, blue: 0,};
+        this.third  = {red: 0,   green: 0,   blue: 255};
+
+        this.r = 248;
+
+        this.offsetX = 250;
+        this.offsetY = 250;
+        
+        this.direction = 1;
+        this.angle = 0;
+    };
+
+    get first_color() {
+        return 'rgb(' + this.first.red + ',' + this.first.green + ',' + this.first.blue + ')';
+    };
+
+    get second_color() {
+        return 'rgb(' + this.second.red + ',' + this.second.green + ',' + this.second.blue + ')';
+    };
+
+    get third_color() {
+        return 'rgb(' + this.third.red + ',' + this.third.green + ',' + this.third.blue + ')';
+    };
+
+    drawIt(ctx) {
+        ctx.fillStyle = this.first_color;
+        ctx.beginPath();
+        ctx.moveTo(this.offsetX, this.offsetY);
+        ctx.arc(this.offsetX, this.offsetY, this.r,
+            this.direction * this.angle + 0, this.direction * this.angle + Math.PI * 2 / 3);
+        ctx.lineTo(this.offsetX, this.offsetY);
+        ctx.fill();
+
+        ctx.fillStyle = this.second_color;
+        ctx.beginPath();
+        ctx.moveTo(this.offsetX, this.offsetY);
+        ctx.arc(this.offsetX, this.offsetY, this.r,
+            this.direction * this.angle + Math.PI * 2 / 3, this.direction * this.angle + Math.PI * 4 / 3);
+        ctx.lineTo(this.offsetX, this.offsetY);
+        ctx.fill();
+
+        ctx.fillStyle = this.third_color;
+        ctx.beginPath();
+        ctx.moveTo(this.offsetX, this.offsetY);
+        ctx.arc(this.offsetX, this.offsetY, this.r,
+            this.direction * this.angle + Math.PI * 4 / 3, this.direction * this.angle + Math.PI * 2);
+        ctx.lineTo(this.offsetX, this.offsetY);
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.arc(this.offsetX, this.offsetY, this.r, 0, Math.PI * 2);
+        ctx.stroke();
+    };
+
+    rotate(speed) {
+        this.angle += speed / 180 * Math.PI;
+    };
+
+};
---
+++ sims/RGB/js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const BOXWIDTH  = 600
+const BOXHEIGHT = 500
+
+
+const toDraw   = []
+const toEvolve = []
+
+const stage = new Stage('boxCanvas', BOXWIDTH, BOXHEIGHT, toDraw, toEvolve)

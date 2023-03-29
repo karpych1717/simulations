839348f717cea00628f863b4b4910143c4260e20
karpych1717 <admkarpych1717@gmail.com>
2023-03-29 15:12:39 +0300

    bore pinck slider

--- sims/RGB/js/core.js
+++ sims/RGB/js/core.js
@@ -283,7 +283,7 @@ class Slider {
     ctx.fillStyle = 'rgb(35, 45, 75)'
     ctx.fillRect(this.offsetX, this.offsetY, this.width, this.height)
 
-    ctx.fillStyle = 'rgb(190, 155, 165)'
+    ctx.fillStyle = 'rgb(250, 60, 200)'
     ctx.beginPath()
     ctx.roundRect(
       this.offsetX + this.padding, this.offsetY + this.padding,
